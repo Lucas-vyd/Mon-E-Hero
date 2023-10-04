330c90aca8e792fc01a426b899036a9f8abc0600
nzopettchaptche <nzopetl@3il.fr>
2023-10-04 15:39:44 +0200

    interface test +boutton

--- app/src/main/java/com/example/mone_hero/MainActivity.java
+++ app/src/main/java/com/example/mone_hero/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mone_hero;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatActivity; // Utilisez ceci pour AppCompatActivity
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonConnect = findViewById(R.id.buttonConnect);
+
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cette méthode sera appelée lorsque le bouton "Se connecter" sera cliqué.
+                Log.d("MainActivity", "Click réussi"); // Affiche "click réussi" dans la console.
+            }
+        });
     }
 }
+
