9e2c21d7585644b84007e86c2f7e64e6dac2f661
Ange <juniorawounfouet@gmail.com>
2023-10-13 11:51:38 +0200

    Création d'un id pour l'application

--- app/src/main/java/com/example/mone_hero/MainActivity.java
+++ app/src/main/java/com/example/mone_hero/MainActivity.java
@@ -6,6 +6,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Context;
+import android.content.SharedPreferences;
+import java.util.UUID;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
 
         Button buttonConnect = findViewById(R.id.buttonConnect);
         final EditText editText = findViewById(R.id.editText); // Référence à votre EditText
+
+        TextView textView = findViewById(R.id.textViewId); // Pour afficher l'id de l'appli
+
+        // Générer un identifiant unique (UUID)
+        UUID uniqueId = UUID.randomUUID();
+        String uniqueIdString = uniqueId.toString();
+
+        // Stocker l'identifiant dans les préférences partagées
+        SharedPreferences preferences = getSharedPreferences("MonAppPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("app_id", uniqueIdString);
+        editor.apply();
+
+        // Afficher l'ID généré dans le TextView
+        textView.setText("ID: "+uniqueIdString);
+
 
         buttonConnect.setOnClickListener(new View.OnClickListener() {
             @Override
